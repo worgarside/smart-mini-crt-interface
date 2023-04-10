9ee3130afddd358b41f952c4fcb75656b31e4c51
worgarside-dev <42623860+worgarside-dev@users.noreply.github.com>
2023-04-10 18:16:01 +0100

    🔄 synced file(s) with worgarside/github-config-files (#73)
    * 🔄 synced local '.pre-commit-config.yaml' with remote '.pre-commit-config.yaml'
    
    * Add actionlint GHA
    
    * 🔄 synced local '.github/workflows/dependabot_auto_merge.yml' with remote '.github/workflows/dependabot_auto_merge.yml'
    
    * 🔄 created local '.github/workflows/pr_autoupdate.yaml' from remote '.github/workflows/pr_autoupdate.yaml'
    
    * 🔄 created local '.github/repo_labels.yml' from remote '.github/repo_labels.yml'
    
    * 🔄 created local '.github/workflows/manage_repo_labels.yml' from remote '.github/workflows/manage_repo_labels.yml'
    
    * 🔄 created local '.github/labeler.yml' from remote '.github/labeler.yml'
    
    * 🔄 created local '.github/workflows/manage_pr_labels.yml' from remote '.github/workflows/manage_pr_labels.yml'
    
    * 🔄 created local '.github/workflows/run_sonarcloud_scan.yml' from remote '.github/workflows/run_sonarcloud_scan.yml'
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix codespell
    
    * Move SonarCloud config
    
    ---------
    
    Co-authored-by: worgarside-dev <null>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- service/install.sh
+++ service/install.sh
@@ -3,17 +3,20 @@
 systemctl stop internal_conditions_reporter.service || :
 cp internal_conditions_reporter.service /etc/systemd/system/
 echo "Service file copied to /etc/systemd/system/internal_conditions_reporter.service"
-systemctl reenable internal_conditions_reporter.service
+systemctl disable internal_conditions_reporter.service
+systemctl enable internal_conditions_reporter.service
 systemctl start internal_conditions_reporter.service
 
 systemctl stop crt_gui.service || :
 cp crt_gui.service /etc/systemd/system/
 echo "Service file copied to /etc/systemd/system/crt_gui.service"
-systemctl reenable crt_gui.service
+systemctl disable crt_gui.service
+systemctl enable crt_gui.service
 systemctl start crt_gui.service
 
 systemctl stop fan_controller.service || :
 cp fan_controller.service /etc/systemd/system/
 echo "Service file copied to /etc/systemd/system/fan_controller.service"
-systemctl reenable fan_controller.service
+systemctl disnable fan_controller.service
+systemctl enable fan_controller.service
 systemctl start fan_controller.service
